e2f2eee4649a4e1cfb7e50e63bd8c9295cde054d
jin <jinlu.hansome@163.com>
2021-02-18 01:01:06 +0800

    提交代码

--- src/views/opensrcinfo/component/busiGraph.vue
+++ src/views/opensrcinfo/component/busiGraph.vue
@@ -6,7 +6,7 @@
             <div class="busiinfo-summary">
               <el-tabs v-model="activeName" @tab-click="handleClick">
                 <el-tab-pane label="总体" name="first">
-                  <el-progress style="margin-bottom: 16px;" v-for="o in 5" :stroke-width="14" :show-text="true" :percentage="70" :format="format"/>
+                  <el-progress style="margin-bottom: 16px;" v-for="o in 5" :stroke-width="14" :show-text="true" :percentage="70" :format="format" :key="o"/>
                 </el-tab-pane>
                 <el-tab-pane label="月度" name="second">月度</el-tab-pane>
               </el-tabs>
@@ -21,7 +21,7 @@
                 prefix-icon="el-icon-search"
                 v-model="input">
               </el-input>
-              <div v-for="o in 5" class="single-detail-info">
+              <div v-for="o in 5" class="single-detail-info" :key="o">
                 <div class="headline">
                   <div class="text-wrapper">
                     <div class="text">
@@ -56,13 +56,49 @@ import { DataSet, Network } from 'vis'
 // 引入uuid文件
 import uuidv1 from 'uuid/v1'
 let network = null
+let DIR = process.env.BASE_URL + 'indonesia/'
 export default {
   data() {
     return {
       activeName: 'first',
       input: '',
-      nodesArray: [{ id: '0', label: '网站订单', title: '网站订单' }],
-      edgesArray: [],
+      nodesArray: [
+        { id: 1, shape: 'circularImage', image: DIR + '1.png', label: '公司名称' },
+        { id: 2, shape: 'circularImage', image: DIR + '2.png', label: '公司名称' },
+        { id: 3, shape: 'circularImage', image: DIR + '3.png', label: '公司名称' },
+        {
+          id: 4,
+          shape: 'circularImage',
+          image: DIR + '4.png',
+          label: '公司名称'
+        },
+        { id: 5, shape: 'circularImage', image: DIR + '5.png', label: '公司名称' },
+        { id: 6, shape: 'circularImage', image: DIR + '6.png', label: '公司名称' },
+        { id: 7, shape: 'circularImage', image: DIR + '7.png', label: '公司名称' },
+        { id: 8, shape: 'circularImage', image: DIR + '8.png', label: '公司名称' },
+        { id: 9, shape: 'circularImage', image: DIR + '9.png', label: '公司名称' },
+        { id: 10, shape: 'circularImage', image: DIR + '10.png', label: '公司名称' },
+        { id: 11, shape: 'circularImage', image: DIR + '11.png', label: '公司名称' },
+        { id: 12, shape: 'circularImage', image: DIR + '12.png', label: '公司名称' },
+        { id: 13, shape: 'circularImage', image: DIR + '13.png', label: '公司名称' },
+        { id: 14, shape: 'circularImage', image: DIR + '14.png', label: '公司名称' }
+      ],
+      edgesArray: [
+        { from: 1, to: 2, label: '关系' },
+        { from: 2, to: 3, label: '关系' },
+        { from: 2, to: 4, label: '关系' },
+        { from: 4, to: 5, label: '关系' },
+        { from: 4, to: 10, label: '关系' },
+        { from: 4, to: 6, label: '关系' },
+        { from: 6, to: 7, label: '关系' },
+        { from: 7, to: 8, label: '关系' },
+        { from: 8, to: 9, label: '关系' },
+        { from: 8, to: 10, label: '关系' },
+        { from: 10, to: 11, label: '关系' },
+        { from: 11, to: 12, label: '关系' },
+        { from: 12, to: 13, label: '关系' },
+        { from: 13, to: 14, label: '关系' }
+      ],
       nodes: null,
       edges: null,
       selectNodeId: ''
@@ -79,10 +115,10 @@ export default {
   },
   methods: {
     handleClick(tab, event) {
-      console.log(tab, event);
+      console.log(tab, event)
     },
     format(percentage) {
-      return percentage;
+      return percentage
     },
     getuuid() {
       return uuidv1()
@@ -117,33 +153,51 @@ export default {
         layout: {
           randomSeed: 2, // 布局的随机种子，如果不设置，每次进来布局都是随机的；这个数值可以任意设置，只要是个数字就行
           improvedLayout: true, // 使用【 Kamada Kawai】布局算法；如果不用这个算法，会出现很多交叉的线条
-          hierarchical: {
-            // enabled: true, // 切换分层布局系统
-            levelSeparation: 200, // 不同级别之间的距离。
-            // nodeSpacing: 100, // 自由轴上节点之间的最小距离,这仅适用于初始布局。如果启用物理，则节点距离将存在有效的节点距离。
-            // treeSpacing: 100, // 不同树木之间的距离（独立网络）
-            // blockShifting: false, // 每个节点都会检查空白，并尽可能地将它的分支与它一起移动，并在任何级别上考虑nodeSpacing
-            // edgeMinimization: false, // 每个节点将尝试沿其自由轴移动以减少其边缘的总长度
-            // parentCentralization: false, // 如果为true，则布局算法完成后，父节点将再次居中。
-            direction: layoutType,
-            sortMethod: 'directed'
-          }
+          // hierarchical: {
+          //   // enabled: true, // 切换分层布局系统
+          //   levelSeparation: 200, // 不同级别之间的距离。
+          //   // nodeSpacing: 100, // 自由轴上节点之间的最小距离,这仅适用于初始布局。如果启用物理，则节点距离将存在有效的节点距离。
+          //   // treeSpacing: 100, // 不同树木之间的距离（独立网络）
+          //   // blockShifting: false, // 每个节点都会检查空白，并尽可能地将它的分支与它一起移动，并在任何级别上考虑nodeSpacing
+          //   // edgeMinimization: false, // 每个节点将尝试沿其自由轴移动以减少其边缘的总长度
+          //   // parentCentralization: false, // 如果为true，则布局算法完成后，父节点将再次居中。
+          //   direction: layoutType,
+          //   sortMethod: 'directed'
+          // }
         },
         physics: {
-          enabled: false // 节点不能重叠,整体图回弹效果
+          enabled: true // 节点不能重叠,整体图回弹效果
         },
         nodes: {
-          shape: 'box',
-          heightConstraint: 20,
-          widthConstraint: 100
+          borderWidth: 4,
+          size: 30,
+          color: {
+            border: '#222222',
+            background: '#666666',
+            hover: {
+              //节点鼠标滑过时状态颜色
+              border: "#edb430",
+              background: "#D2E5FF"
+            }
+          },
+          font: { color: '#606c74' }
         },
         edges: {
-          arrows: 'to',
+          width: 1,
+          length: 260,
           color: {
-            color: '#2B7CE9',
-            highlight: 'red',
-            hover: 'red'
-          }
+            color: '#848484',
+            highlight: '#848484',
+            hover: '#848484',
+            inherit: 'from',
+            opacity: 1.0
+          },
+          shadow: true,
+          smooth: {
+            // 设置两个节点之前的连线的状态
+            enabled: true // 默认是true，设置为false之后，两个节点之前的连线始终为直线，不会出现贝塞尔曲线
+          },
+          arrows: { to: true } // 箭头指向to
         },
         interaction: {
           dragNodes: true,
@@ -162,17 +216,18 @@ export default {
       network = new Network(this.$refs.visualization, data, options)
 
       // add event listeners
-      network.on('selectNode', params => {
-        console.log(params)
+      network.on('selectNode', e => {
+        this.selectNodeId = e.nodes[0] + ''
       })
-      network.on('showPopup', id => {
-        this.selectNodeId = id
+      network.on('hoverNode', e => {
+        console.log(e)
+        this.selectNodeId = e.node
       })
-      network.on('hidePopup', id => {
+      network.on('blurNode', e => {
         this.selectNodeId = ''
       })
       network.on('oncontext', params => {
-        if (this.selectNodeId.length === 0) {
+        if (this.selectNodeId === '') {
           this.selectNodeId = ''
           params.event.preventDefault()
           params.event.stopPropagation()
@@ -189,7 +244,6 @@ export default {
     },
     popupMenu(params) {
       const toNodeId = this.selectNodeId
-      this.selectNodeId = ''
       this.$contextmenu({
         items: [
           { label: '添加',
@@ -226,24 +280,21 @@ export default {
     addNode(toNodeId) {
       const nodeId = this.getuuid()
       const edgeId = this.getuuid()
-      this.nodes.add({ id: nodeId, label: '用户消费', title: '用户消费' })
-      this.nodesArray.push({ id: nodeId, label: '用户消费', title: '用户消费' })
+      this.nodes.add({ id: nodeId,  shape: 'circularImage', image: DIR + '14.png', label: '公司名称' })
+      this.nodesArray.push({ id: nodeId,  shape: 'circularImage', image: DIR + '14.png', label: '公司名称' })
       this.edges.add({
         id: edgeId,
         from: nodeId,
-        to: toNodeId,
-        label: 'left join'
+        to: toNodeId
       })
       this.edgesArray.push({
         id: edgeId,
         from: nodeId,
-        to: toNodeId,
-        label: 'left join',
-        arrows: { to: true }
+        to: toNodeId
       })
     },
     _delChildNodes(network, thisNode) {
-      const nodes = network.getConnectedNodes(thisNode, 'from')
+      const nodes = network.getConnectedNodes(thisNode, 'to')
       if (nodes.length > 0) {
         for (let i = 0; i < nodes.length; i++) {
           this._delChildNodes(network, nodes[i])
@@ -265,7 +316,7 @@ export default {
     width: 331px;
     margin-left: 13px;
     margin-bottom: 13px;
-    padding: 0px 0px 12px 12px;
+    padding: 0px 0px 5px 12px;
     flex: 1 0 331px;
     background: #FFFFFF;
     border: 1px solid #E7E8F2;
@@ -281,8 +332,8 @@ export default {
       .single-detail-info {
         width: 100%;
         border-bottom: 1px solid #e4e4e4;
-        padding-top: 16px;
-        padding-bottom: 12px;
+        padding-top: 5px;
+        padding-bottom: 5px;
         .headline {
           width: 100%;
           .text-wrapper {
@@ -317,7 +368,7 @@ export default {
         .content-wrapper {
           width: 100%;
           .one-content {
-            width: calc(100% - 60px);
+            width: calc(100% - 10px);
             font-family: PingFangSC-Regular;
             font-size: 14px;
             color: rgba(0,0,0,0.45);
@@ -348,7 +399,6 @@ export default {
             display: flex;
             justify-content: space-between;
             align-items: center;
-            margin-top: 10px;
             .part-one {
               width: 500px;
               .text {
